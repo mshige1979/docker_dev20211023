a063a783577195ff1687c4f959904f360b13c952
mshige1979 <mshige1979@ryuouen.net>
2021-10-29 23:50:10 +0900

    DB初期設定ファイル修正

--- services/db/sql/00_init.sql
+++ services/db/sql/00_init.sql
@@ -1,2 +1,18 @@
+-- データベースの作成
+CREATE DATABASE sample1_dev;
+CREATE DATABASE sample1_staging;
+CREATE DATABASE sample1_test;
+CREATE DATABASE sample1_prod;
+
 -- PostGISの拡張機能
-CREATE EXTENSION IF NOT EXISTS postgis;
+\c sample1_dev;
+CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
+
+\c sample1_staging;
+CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
+
+\c sample1_test;
+CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
+
+\c sample1_prod;
+CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
